ba560eaeb77f740a664726539330c6230206d2de
Joel Santos <joeltere9@gmail.com>
2023-03-04 14:18:47 -0300

    comand 2

---
+++ src/config/database.ts
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "joel",
+    database: "barber",
+    define: {
+      timespamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  };
